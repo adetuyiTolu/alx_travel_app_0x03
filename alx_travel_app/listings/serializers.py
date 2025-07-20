bcbeecb461a619d8ed339904da1fc2ff0cc0e537
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-20 13:11:08 -0400

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -5,9 +5,11 @@ class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at', 'listing_id']
 
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+        read_only_fields = ['created_at', 'booking_id']
