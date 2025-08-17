3b2019465e51cf0be69174a5f73276da0536cc49
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-08-17 13:32:49 -0400

    create payment model

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -51,3 +51,24 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.rating}/5 by {self.user.email}"
+
+
+class Payment(models.Model):
+    
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    )
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='payment')
+    transaction_id = models.CharField(max_length=100, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES, default='Pending')  # Pending, Completed, Failed
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Payment {self.transaction_id} for booking {self.booking.id}"
+
+    
